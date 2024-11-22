ab5566a55e7ebebed7e5be283290a702ca1bc557
MXSN18 <max.stavenhagen@gmail.com>
2024-11-22 20:39:22 +0100

    Änderungen VSCode

--- pythonApp3.py
+++ pythonApp3.py
@@ -14,6 +14,12 @@ path = '/tmp/exampleRepository/dataset03.csv'
 data = pd.read_csv(path)
 
 # Step 2: Data Cleaning
+data['x'] = data['x'] = pd.to_numeric(data['x'], errors='coerce')
 
+data['y'] = data['y'] = pd.to_numeric(data['y'], errors='coerce')
+
+data_clean = data.dropna()
+
+#Step 3: Z-scores, 
 
 
